f596209f7a07b0339825a068bdadc8dab9e3b4d7
Prems303 <premsagarthogaru@yahoo.com>
2016-06-21 19:06:12 +1200

    added showcell function

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,17 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 function startGame () {
+   var boardCells = document.getElementsByClassName('board')[0].children;
+    for (var i = 0; i < boardCells.length; i ++) {
+     addListeners(boardCells[i])
+    }
+}
+
+function addListeners(element) {
+  element.addEventListener('click', showCell);
+
+}
+
+function showCell(evt) {
+  evt.target.classList.remove('hidden');
 }
